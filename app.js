bb7345a2b7daeabdf7928a075fffcd577a79a693
Kassmasse01 <kasget1@gmail.com>
2023-03-16 17:15:53 +0300

    app.js-fix

--- app.js
+++ app.js
@@ -2,11 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req,res) =>{
-<<<<<<< HEAD
-    res.send('hello API!');
-=======
-    res.send('hello API!')
->>>>>>> 4c3ecbf (video-15-creating-the-backend-server-with-express)
+    res.send('hello')
 })
 
 app.listen(3000, ()=>{
