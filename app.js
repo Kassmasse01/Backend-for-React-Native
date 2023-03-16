ca59dbee07dfed705b3b6230a967dcd9b1ba338c
Kassmasse01 <kasget1@gmail.com>
2023-03-16 13:46:37 +0300

    video-15-creating-the-backend-server-with-express

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req,res) =>{
+<<<<<<< HEAD
     res.send('hello API!');
+=======
+    res.send('hello API!')
+>>>>>>> 4c3ecbf (video-15-creating-the-backend-server-with-express)
 })
 
 app.listen(3000, ()=>{
