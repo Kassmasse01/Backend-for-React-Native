2e0e282f66661f5a7954ea75875e6a549affc0d1
Kassmasse01 <kasget1@gmail.com>
2023-03-17 12:43:21 +0300

    video-20-write-to-DB-using-API

--- app.js
+++ app.js
@@ -4,28 +4,51 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 
+
 //middleware
 app.use(bodyParser.json());
 app.use(morgan('tiny'));
+
+const productSchema = mongoose.Schema({
+    title: { type: String, required: true },
+    image: { type: String, required: true }
+    // price: { type: Number, required: true },
+    // long_description: { type: String, required: true },
+    // short_description: { type: String, required: true },
+    // category: { type: String, required: true },
+})
+
+const Product = mongoose.model('Product', productSchema);
 
 require('dotenv/config');
 
 const api = process.env.API_URL;
 
 //http://localhost:3000/api/v1/products
-app.get(`${api}/products`, (req,res) =>{
-    const product = {
-        id: 1,
-        name: 'hair dresser',
-        image: 'some_url',
+app.get(`${api}/products`, async (req,res) =>{
+    const productList = await Product.find();
+
+    if(!productList) {
+        res.status(500).json({success: false})
     }
-    res.send(product);
+
+    res.send(productList);
 })
 
 app.post(`${api}/products`, (req,res) =>{
-    const newProduct = req.body;
-    console.log(newProduct);  
-    res.send(newProduct);
+    const product = new Product({
+        title : req.body.title,
+        image : req.body.image
+    })
+
+    product.save().then((createdProduct=>{
+        res.status(201).json(createdProduct)
+    })).catch((err)=>{
+        res.status(500).json({
+            error: err,
+            success: false
+        })
+    })
 })
 
 mongoose.connect(process.env.MONGODB_URI)
